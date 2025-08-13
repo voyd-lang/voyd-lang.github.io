d466c016379825c11b62cc2e721804156af80aef
Drew Youngwerth <drew@youngwerth.com>
2025-08-12 19:14:58 -0700

    Allow top level await

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  esbuild: {
+    supported: {
+      "top-level-await": true, //browsers can handle top-level-await features
+    },
+  },
 });
